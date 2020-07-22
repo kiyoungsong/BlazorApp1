6e35d9e9d0d0619c7e8b40a026e00b16c6baefd4
kiyoungsong <thdrldud369@gmail.com>
2020-07-22 23:28:00 +0900

    Behind Code Add

---
+++ BlazorApp1/Pages/About.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BlazorApp1.Pages
+{
+    public partial class About
+    {
+        private string title = "정보";
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string subTitle = "사이트 정보";
+
+        public string SubTitle
+        {
+            get { return subTitle; }
+            set { subTitle = value; }
+        }
+
+        protected override void OnInitialized()
+        {
+            subTitle = DateTime.Now.ToLongTimeString();
+        }
+
+    }
+}
---
+++ BlazorApp1/Pages/Services.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1.Pages
+{
+    public partial class Services
+    {
+        public string Title { get; set; } = "타이틀";
+        protected override void OnInitialized()
+        {
+            
+        }
+    }
+}
